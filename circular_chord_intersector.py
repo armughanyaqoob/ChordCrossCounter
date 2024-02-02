791c13f7ac2bcba8c7a9ad52ecc7ed83783cc884
M. Armughan Yaqoob <64024046+armughanyaqoob@users.noreply.github.com>
2024-02-02 11:50:34 +0500

    Create circular_chord_intersector.py

---
+++ circular_chord_intersector.py
@@ -0,0 +1,29 @@
+from math import pi
+
+def count_intersections(rads, identifiers):
+    n = len(rads)
+    intersections = 0
+    active_chords = 0
+
+    for i in range(n):
+        if identifiers[i].startswith('s'):
+        active_chords += 1
+        else:
+            intersections += active_chords * (active_chords - 1) // 2
+            active_chords -= 1
+    active_chords += 1
+        else:
+            intersections += active_chords * (active_chords - 1) // 2
+            active_chords -= 1
+
+    return intersections
+
+rads = [0.78, 1.47, 1.77, 3.92]
+identifiers = ["s1", "s2", "e1", "e2"]
+intersections = count_intersections(rads, identifiers)
+print(f"Number of intersections: {intersections}")
+
+rads = [0.9, 1.3, 1.70, 2.92]
+identifiers = ["s1", "e1", "s2", "e2"]
+intersections = count_intersections(rads, identifiers)
+print(f"Number of intersections: {intersections}")
